7e0b3b59014d805a66284115557048ac2f51adcb
sunnysingh44-coder <singhsunny1044@gmail.com>
2025-04-02 07:03:06 +0000

    modified app.py and home.html

--- app.py
+++ app.py
@@ -2,11 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India',
+        'salary': 'Rs. 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': 'Rs. 12,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$120,000'
+    },
+]
 
 @app.route("/")
-def hello():
-  return render_template('home.html')
-
+def home():
+    return render_template('home.html', jobs=JOBS, company_name='Sunny')
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
+
